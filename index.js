fd241f90b777373e2f9fd5e90c3a12c4c0bd44a9
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-11-08 04:38:03 +0600

    added orderCollection to store  orders

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 //mongodb connection
 
@@ -27,6 +35,44 @@ async function run() {
     await client.connect();
 
     const foodCollection = await client.db("naamkeenDB").collection("foods");
+    const userCollection = await client.db("naamkeenDB").collection("users");
+    const orderCollection = await client
+      .db("naamkeenDB")
+      .collection("orderItems");
+
+    const verifyToken = (req, res, next) => {
+      const { token } = req.cookies;
+      if (!token) {
+        return res.status(401).send({ message: "You are not authorized" });
+      }
+
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+          if (err) {
+            return res.status(401).send({ message: "You are not authorized" });
+          }
+          req.user = decoded;
+          next();
+        }
+      );
+    };
+
+    //jwt
+    app.post("/api/v1/auth/access-token", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: 60 * 60,
+      });
+      console.log(token);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     //get top 6 foods
     app.get("/api/v1/foods", async (req, res) => {
@@ -36,6 +82,7 @@ async function run() {
       const category = req.query.foodcategory;
       const sortField = req.query.sortField;
       const sortOrder = req.query.sortOrder;
+
       if (category) {
         query.foodcategory = category;
       }
@@ -44,7 +91,58 @@ async function run() {
       }
 
       const cursor = foodCollection.find(query).sort(sortObj);
+
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //pages
+    app.get("/api/v1/foods/pages", async (req, res) => {
+      //pagination
+      const page = Number(req.query.page);
+      const limit = Number(req.query.limit);
+      const skip = (page - 1) * limit;
+      const cursor = foodCollection.find().skip(skip).limit(limit);
+      const total = await foodCollection.countDocuments();
+      const result = await cursor.toArray();
+      res.send({ total, result });
+    });
+
+    app.post("/api/v1/users", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userCollection.insertOne(userInfo);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/api/v1/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/api/v1/user/create-order", async (req, res) => {
+      const orderItem = req.body;
+
+      const result = await orderCollection.insertOne(orderItem);
+      res.send(result);
+    });
+
+    app.get("/api/v1/user/create-order", verifyToken, async (req, res) => {
+      const queryEmail = req.query.email;
+      const tokenEmail = req.user.email;
+      if (queryEmail !== tokenEmail) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+
+      const result = await orderCollection.findOne({ email: queryEmail });
+      res.send(result);
+    });
+    app.delete("/api/v1/user/delete-order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
 
